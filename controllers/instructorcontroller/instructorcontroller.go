1fae7eb5bfdeff889f933a6632ac0a1f6d32c5aa
christoperBar <cbarnaby@student.ciputra.ac.id>
2024-01-14 16:44:35 +0700

    feat: instructor list, instructor details

---
+++ controllers/instructorcontroller/instructorcontroller.go
@@ -0,0 +1,98 @@
+package instructorcontroller
+
+import (
+	// "fmt"
+	"net/http"
+
+	"github.com/christoperBar/WeLearnAPI/models"
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+type InstructorExpertiseDTO struct {
+	Id   int64  `json:"id"`
+	Name string `json:"name"`
+}
+
+type InstructorListDTO struct {
+	Id         int64                    `json:"id"`
+	AuthId     string                   `json:"authid"`
+	DOB        string                   `json:"dob"`
+	Address    string                   `json:"address"`
+	Phone      string                   `json:"phone"`
+	Image_url  string                   `json:"image_url"`
+	Expertises []InstructorExpertiseDTO `json:"expertises"`
+}
+
+func InstructorList(c *fiber.Ctx) error {
+	var instrucors []models.Instructor
+
+	if err := models.DB.Preload("Expertises").Find(&instrucors).Error; err != nil {
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Internal Server Error",
+		})
+	}
+
+	var instructorsDTO []InstructorListDTO
+	for _, instructor := range instrucors {
+
+		var expertisesDTO []InstructorExpertiseDTO
+		for _, expertise := range instructor.Expertises {
+			expertisesDTO = append(expertisesDTO, InstructorExpertiseDTO{
+				Id:   expertise.Id,
+				Name: expertise.Name,
+			})
+		}
+
+		instructorDTO := InstructorListDTO{
+			Id:         instructor.Id,
+			AuthId:     instructor.AuthId,
+			DOB:        instructor.DOB,
+			Address:    instructor.Address,
+			Phone:      instructor.Phone,
+			Image_url:  instructor.Image_url,
+			Expertises: expertisesDTO,
+		}
+
+		instructorsDTO = append(instructorsDTO, instructorDTO)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(instructorsDTO)
+}
+
+func InstructorDetail(c *fiber.Ctx) error {
+
+	id := c.Params("id")
+
+	var instructor models.Instructor
+
+	if err := models.DB.Preload("Expertises").Where("id = ?", id).First(&instructor).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return c.Status(http.StatusNotFound).JSON(fiber.Map{
+				"message": "Sayembara not found",
+			})
+		}
+		return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Internal Server Error",
+		})
+	}
+
+	var expertisesDTO []InstructorExpertiseDTO
+	for _, expertise := range instructor.Expertises {
+		expertisesDTO = append(expertisesDTO, InstructorExpertiseDTO{
+			Id:   expertise.Id,
+			Name: expertise.Name,
+		})
+	}
+
+	instructorDetails := fiber.Map{
+		"id":         instructor.Id,
+		"authid":     instructor.AuthId,
+		"dob":        instructor.DOB,
+		"address":    instructor.Address,
+		"phone":      instructor.Phone,
+		"image_url":  instructor.Image_url,
+		"expertises": expertisesDTO,
+	}
+	return c.Status(fiber.StatusOK).JSON(instructorDetails)
+}
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/christoperBar/WeLearnAPI/controllers/instructorcontroller"
 	"github.com/christoperBar/WeLearnAPI/controllers/studentcontroller"
 	"github.com/christoperBar/WeLearnAPI/models"
 	"github.com/gofiber/fiber/v2"
@@ -12,15 +13,19 @@ func main() {
 
 	app := fiber.New()
 
+	//students
 	app.Post("/api/students/register", studentcontroller.Register)
 	app.Get("/api/students/:id", studentcontroller.UserProfile)
 
+	//sayembara
 	app.Get("/api/students/:id/sayembaras", studentcontroller.SayembaraList)
 	app.Get("/api/students/:id/sayembaras/:sayembaraid", studentcontroller.SayembaraDetail)
-
-	//todo
 	// app.Put("/api/students/:id/sayembaras/:sayembaraid", studentcontroller.EditStatussayembara)
 	app.Post("/api/students/:id/sayembaras/", studentcontroller.CreateSayembara)
+
+	//instrucors
+	app.Get("/api/instructors", instructorcontroller.InstructorList)
+	app.Get("/api/instructors/:id", instructorcontroller.InstructorDetail)
 
 	app.Listen(":8000")
 }
